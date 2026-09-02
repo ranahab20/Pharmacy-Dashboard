a1d52dff903bab2bdf57fe4273b5f307559e6d24
ranahab20 <ranahabta@gmail.com>
2026-09-02 23:31:27 +0200

    link customers page with backend

--- src/pages/Customers/Customers.jsx
+++ src/pages/Customers/Customers.jsx
@@ -1,66 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../../components/Card/Card";
 import { IoPeople } from "react-icons/io5";
 import "./Customers.css";
+import api from "../../api/axiosInstance";
+import toast from "react-hot-toast";
+import Loading from "../Loading/Loading";
 
-const initialCustomers = [
-  {
-    user_id: 1,
-    full_name: "محمد علي أحمد",
-    phone: "0977553366",
-    created_at: "2026-05-12",
-    created_time: "10:30 ص",
-    email: "mohammad@gmail.com",
-    status: "active",
-  },
-  {
-    user_id: 2,
-    full_name: "سارة محمود",
-    phone: "0933445566",
-    created_at: "2026-05-14",
-    created_time: "09:15 ص",
-    email: "sara@gmail.com",
-    status: "active",
-  },
-  {
-    user_id: 3,
-    full_name: "أحمد خالد",
-    phone: "0955667788",
-    created_at: "2026-05-18",
-    created_time: "01:20 م",
-    email: "ahmad@gmail.com",
-    status: "inactive",
-  },
-  {
-    user_id: 4,
-    full_name: "نور علي",
-    phone: "0944778899",
-    created_at: "2026-06-02",
-    created_time: "11:45 ص",
-    email: "nour@gmail.com",
-    status: "active",
-  },
-  {
-    user_id: 5,
-    full_name: "ليان محمد",
-    phone: "0988112233",
-    created_at: "2026-06-08",
-    created_time: "03:10 م",
-    email: "layan@gmail.com",
-    status: "inactive",
-  },
-  {
-    user_id: 6,
-    full_name: "عمر إبراهيم",
-    phone: "0966332211",
-    created_at: "2026-06-15",
-    created_time: "08:55 ص",
-    email: "omar@gmail.com",
-    status: "active",
-  },
-];
 const Customers = () => {
-  const [customers, setCustomers] = useState(initialCustomers);
+  const [customers, setCustomers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [totalCustumers, setTotalCustomers] = useState(0);
+  const [activeCustomers, setActiveCustomers] = useState(0);
+  const [inactiveCustomers, setInactiveCustomers] = useState(0);
+
+  useEffect(() => {
+    const fetchCustomers = async () => {
+      try {
+        setLoading(true);
+        const response = await api.get("/customers");
+
+        console.log("customers:", response.data);
+
+        setCustomers(response.data.data);
+
+        setTotalCustomers(response.data.stats.total);
+        setActiveCustomers(response.data.stats.active);
+        setInactiveCustomers(response.data.stats.inactive);
+      } catch (error) {
+        console.error("Error fetching customers:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCustomers();
+  }, []);
+
+  const deleteCtm = async (id) => {
+    try {
+      const response = await api.delete(`/customers/${id}`);
+      console.log("Delete response:", response.data);
+      toast.success(response.data.message);
+      setCustomers((prevCustomers) =>
+        prevCustomers.map((customer) =>
+          customer.id === id
+            ? {
+                ...customer,
+                is_active: false,
+              }
+            : customer,
+        ),
+      );
+    } catch (error) {
+      console.error("Error deleting customer:", error);
+      toast.error(error.response?.data?.message);
+    }
+  };
+
+  if (loading) {
+    return <Loading text="جاري تحميل العملاء..." />;
+  }
   return (
     <>
       <div className="ctm-div">
@@ -72,19 +71,19 @@ const Customers = () => {
             icon={<IoPeople />}
             title="عملاء نشطون "
             className="ctm-card1"
-            data="10"
+            data={activeCustomers}
           />
           <Card
             icon={<IoPeople />}
             title=" عملاء غير نشطين"
             className="ctm-card2"
-            data="10"
+            data={inactiveCustomers}
           />
           <Card
             icon={<IoPeople />}
             title="إجمالي العملاء"
             className="ctm-card3"
-            data="10"
+            data={totalCustumers}
           />
         </div>
         <div className="ctm-table">
@@ -102,27 +101,36 @@ const Customers = () => {
 
             <tbody>
               {customers.map((customer) => (
-                <tr key={customer.user_id}>
-                  <td>{customer.user_id}</td>
+                <tr
+                  key={customer.id}
+                  className={customer.is_active ? "" : "inactive-customer-row"}
+                >
+                  <td>{customer.id}</td>
 
-                  <td>{customer.full_name}</td>
+                  <td>{customer.name}</td>
                   <td>{customer.phone}</td>
                   <td>{customer.email}</td>
                   <td>
                     <span
                       className={
-                        customer.status === "active"
+                        customer.is_active === true
                           ? "customer-active"
                           : "customer-inactive"
                       }
                     >
-                      {customer.status === "active" ? "نشط" : "غير نشط"}
+                      {customer.is_active === true ? "نشط" : "غير نشط"}
                     </span>
                   </td>
 
                   <td>
                     {" "}
-                    <button className="delete-btn">🗑️</button>
+                    <button
+                      className="delete-btn"
+                      onClick={() => deleteCtm(customer.id)}
+                      disabled={!customer.is_active}
+                    >
+                      🗑️
+                    </button>
                   </td>
                 </tr>
               ))}
